f332c95593c72c43986870ae9af007569341b145
OOOOOOOOOOOOOOOF6357 <66689845+OOOOOOOOOOOOOOOF6357@users.noreply.github.com>
2022-11-09 20:35:05 -0300

    Add files via upload

---
+++ Atividade 07/script.js
@@ -0,0 +1,8 @@
+let aatual, nasc, nome, idade, alerta;
+aatual = '2022';
+nasc = '2004';
+nome = 'Jorge';
+idade = aatual - nasc;
+alerta = `Olá, seu nome é ${nome} e você tem ${idade} anos`
+
+alert(`${alerta}`)
